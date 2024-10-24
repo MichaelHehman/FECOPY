058f7f497bc620c1da710221bf8c627a4f59a7d6
Nhi Nguyen <nhing17899@gmail.com>
2024-10-24 13:49:27 -0400

    fix: update routes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import './App.css';
 import Activities from './pages/ActivitiesPage/Activities';
 
 import {
-  createBrowserRouter,
-  RouterProvider,
-  BrowserRouter,
+  Routes,
+  Route,
 } from "react-router-dom";
 import Home from './pages/HomePage/Home';
 import Todo from './pages/ToDoPage/Todo';
@@ -12,50 +11,25 @@ import Profile from './pages/ProfilePage/Profile';
 import AddTask from './pages/AddTaskPage/AddTask';
 import Navbar from './components/NavBar';
 import { Layout } from 'antd';
-import styles from './App.css';
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/dashboard",
-      element: <Home />,
-    },
-    {
-      path: "/activities",
-      element: <Activities />,
-    },
-    {
-      path: "/add-task",
-      element: <AddTask />,
-    },
-    {
-      path: "/to-do",
-      element: <Todo />,
-    },
-    {
-      path: "/profile",
-      element: <Profile />,
-    }
-  ]);
-
   return (
     <Layout className="container">
       <Layout.Header className="header">
-        <div>
-          Split Chores
-        </div>
+        Split Chores
       </Layout.Header>
       <Layout.Content className="main">
-        <RouterProvider router={router} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/activities" element={<Activities />} />
+          <Route path="/add-task" element={<AddTask />} />
+          <Route path="/to-do" element={<Todo />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
       </Layout.Content>
       <Layout.Footer className="footer">
-        <BrowserRouter>
-          <Navbar />
-        </BrowserRouter>
+        <Navbar />
       </Layout.Footer>
     </Layout>
   );
