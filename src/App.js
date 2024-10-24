057c6e171cd3f5c3a5c61878d03b7f2d3d58e43b
Nhi Nguyen <nhing17899@gmail.com>
2024-10-24 13:15:08 -0400

    feat: basic layout + routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import Activities from './pages/ActivitiesPage/Activities';
-import Home from './pages/HomePage/Home';
 
 import {
   createBrowserRouter,
   RouterProvider,
   BrowserRouter,
 } from "react-router-dom";
+import Home from './pages/HomePage/Home';
 import Todo from './pages/ToDoPage/Todo';
 import Profile from './pages/ProfilePage/Profile';
 import AddTask from './pages/AddTaskPage/AddTask';
+import Navbar from './components/NavBar';
+import { Layout } from 'antd';
+import styles from './App.css';
 
 function App() {
   const router = createBrowserRouter([
@@ -40,9 +43,21 @@ function App() {
   ]);
 
   return (
-    <div className="container">
-      <RouterProvider router={router} />
-    </div>
+    <Layout className="container">
+      <Layout.Header className="header">
+        <div>
+          Split Chores
+        </div>
+      </Layout.Header>
+      <Layout.Content className="main">
+        <RouterProvider router={router} />
+      </Layout.Content>
+      <Layout.Footer className="footer">
+        <BrowserRouter>
+          <Navbar />
+        </BrowserRouter>
+      </Layout.Footer>
+    </Layout>
   );
 }
 
