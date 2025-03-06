815c205fc366d69643264e180fd68b6ac5b596fc
michaelhehman <mike1hehman@gmail.com>
2025-03-06 16:11:17 -0500

    Update NavBar and Navbar.module.css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,33 +5,33 @@ import styles from './NavBar.module.css';
 function NavBar() {  
     return (  
         <nav className={styles.nav}>  
-            <NavLink   
-                to="/"   
+            <NavLink  
+                to="/"  
                 className={({ isActive }) => `${styles.navBtn} ${isActive ? styles.active : ''}`}  
                 end  
             >  
                 <img className={styles.icon} src="/assets/images/calendarIcon.png" alt="dashboard" />  
             </NavLink>  
-            <NavLink   
-                to="/activities"   
+            <NavLink  
+                to="/activities"  
                 className={({ isActive }) => `${styles.navBtn} ${isActive ? styles.active : ''}`}  
             >  
                 <img className={styles.icon} src="/assets/images/activitiesIcon.png" alt="activities" />  
             </NavLink>  
-            <NavLink   
-                to="/add-task"   
+            <NavLink  
+                to="/add-task"  
                 className={({ isActive }) => `${styles.navBtn} ${isActive ? styles.active : ''}`}  
             >  
                 <img className={styles.icon} src="/assets/images/addIcon.png" alt="add task" />  
             </NavLink>  
-            <NavLink   
-                to="/to-do"   
+            <NavLink  
+                to="/to-do"  
                 className={({ isActive }) => `${styles.navBtn} ${isActive ? styles.active : ''}`}  
             >  
                 <img className={styles.icon} src="/assets/images/todoIcon.png" alt="to do" />  
             </NavLink>  
-            <NavLink   
-                to="/profile"   
+            <NavLink  
+                to="/profile"  
                 className={({ isActive }) => `${styles.navBtn} ${isActive ? styles.active : ''}`}  
             >  
                 <img className={styles.icon} src="/assets/images/profileIcon.png" alt="profile" />  
